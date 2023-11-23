1a4c6c3bb255e7d999e6979806723bfb16b1fefa
navyasharma0203 <73904513+navyasharma0203@users.noreply.github.com>
2023-11-23 22:05:52 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-1_sqwLNQTj8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gsfafg5eu353o92tbp566cndf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://rqe240ss00.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
